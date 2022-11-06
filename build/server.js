50359560b2fed882d67b2089b95b3d634d09062a
Maksim Lonshakov <lonshakov.max@gmail.com>
2022-11-06 22:46:36 +0300

    try to deploy

---
+++ build/server.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+      path = require('path'),
+      PORT = process.env.PORT || 3000;
+
+
+const app = express()
+
+app.use('/', express.static(__dirname))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+})
